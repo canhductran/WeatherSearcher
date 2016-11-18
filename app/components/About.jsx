ee777478512d016c4b8c699b38d534b3a59c5b9f
Canh Duc Tran <CHRIS@Canhs-MacBook-Pro.local>
2016-11-19 02:15:34 +1100

    Backup

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,14 @@ var React = require('react');
 var About = (props) => {
   return(
     <div>
-      <h3>About</h3>
-      <p>Welcome to the About page</p>
+      <h3 className="text-center">About</h3>
+      <p>This is a weather application built in React</p>
+      <p>Some of them are:</p>
+      <ul>
+        <li><a href="https://facebook.github.io/react">React</a></li>
+        <li><a href="https://openweathermap.org/">OpenWeatherMap</a></li>
+        <li><a href="http://foundation.zurb.com/">Foundation</a></li>
+      </ul>
     </div>
   )
 };
--- public/bundle.js
+++ public/bundle.js
@@ -26664,23 +26664,59 @@
 /* 254 */
 /***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
+	"use strict";
 
 	var React = __webpack_require__(8);
 
 	var About = function About(props) {
 	  return React.createElement(
-	    'div',
+	    "div",
 	    null,
 	    React.createElement(
-	      'h3',
-	      null,
-	      'About'
+	      "h3",
+	      { className: "text-center" },
+	      "About"
 	    ),
 	    React.createElement(
-	      'p',
+	      "p",
 	      null,
-	      'Welcome to the About page'
+	      "This is a weather application built in React"
+	    ),
+	    React.createElement(
+	      "p",
+	      null,
+	      "Some of them are:"
+	    ),
+	    React.createElement(
+	      "ul",
+	      null,
+	      React.createElement(
+	        "li",
+	        null,
+	        React.createElement(
+	          "a",
+	          { href: "https://facebook.github.io/react" },
+	          "React"
+	        )
+	      ),
+	      React.createElement(
+	        "li",
+	        null,
+	        React.createElement(
+	          "a",
+	          { href: "https://openweathermap.org/" },
+	          "OpenWeatherMap"
+	        )
+	      ),
+	      React.createElement(
+	        "li",
+	        null,
+	        React.createElement(
+	          "a",
+	          { href: "http://foundation.zurb.com/" },
+	          "Foundation"
+	        )
+	      )
 	    )
 	  );
 	};
